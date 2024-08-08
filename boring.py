cc27f9a7ab488dc9a5652d8105eb7ca01210a015
rcssntg <rcssntg@gmail.com>
2024-08-08 13:18:51 +0800

    testing commit exit in vs code

--- boring.py
+++ boring.py
@@ -20,4 +20,6 @@ print(len(myName))
 
 print('What is your age?')    # ask for their age
 myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')  
+
+# i am trying to push using git bash
