97a54a87e048bc577d6fdfa8c21a3be377efaf8b
rcssntg <rcssntg@gmail.com>
2024-08-10 15:56:53 +0800

    truthy and falsey

--- boring.py
+++ boring.py
@@ -96,13 +96,26 @@
     
 # CONTINUE
 
-while True:
-    print('Who are you?')
+# while True:
+#     print('Who are you?')
+#     name = input()
+#     if name != 'Joe':
+#         continue
+#     print('Hello, Joe. What is the password? (It is fish.)')
+#     password = input()
+#     if password == 'swordfish':
+#         break
+# print('Access granted')
+
+
+# Truthy and Falsey
+name = ''
+
+while not name:
+    print('Enter your name:')
     name = input()
-    if name != 'Joe':
-        continue
-    print('Hello, Joe. What is the password? (It is fish.)')
-    password = input()
-    if password == 'swordfish':
-        break
-print('Access granted')
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests:
+    print('Be sure to have enough room for all your guests.')
+print('Done!')
