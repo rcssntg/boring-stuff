3fb3f03baf5e0d925021fb8062209fe9ecf2fd3a
rcssntg <rcssntg@gmail.com>
2024-08-10 10:34:27 +0800

    alice

--- boring.py
+++ boring.py
@@ -28,3 +28,35 @@
 
 # Chapter 2 FLOW CONTROL
 
+# print('Is raining? y or n')
+
+# answer = input()
+
+# if answer == 'y':
+#     print('Have umbrella? y or n')
+#     umbrella = input()
+# elif umbrella == 'y':
+#         print('Go outside')
+# else:
+#     print('wait a while')
+
+
+
+# name = input('What is your name? ')
+# if name == 'Mary':
+#     print('Hello Mary')  
+ 
+# password = input('What is your password? ')    
+# if password == 'swordfish':
+#     print('Access granted.')
+# else:
+#     print('Wrong password')
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire')
+elif age > 100:
+    print('You are not Alice, grannie.')
