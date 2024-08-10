e530584f0f2d02b39b3c798eb375e53c054fd296
rcssntg <rcssntg@gmail.com>
2024-08-10 16:33:25 +0800

    Equivalent Loop

--- boring.py
+++ boring.py
@@ -136,3 +136,12 @@
 # for num in range(101):
 #     total = total + num
 # print(f'The total is {total}')
+
+# Equivalent Loop
+# print('My name is')
+# i = 0
+# while i < 5:
+#     print('Jimmy Five Times (' + str(i) + ')')
+#     i = i + 1
+    
+    
