223d7c2548beb108b23290f2d39bde6919355471
rcssntg <rcssntg@gmail.com>
2024-08-10 15:37:23 +0800

    annoying loop

--- boring.py
+++ boring.py
@@ -52,11 +52,40 @@
 # else:
 #     print('Wrong password')
 
-if name == 'Alice':
-    print('Hi Alice')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire')
-elif age > 100:
-    print('You are not Alice, grannie.')
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif age < 12:
+#     print('You are not Alice, kiddo.')
+# elif age > 2000:
+#     print('Unlike you, Alice is not an undead, immortal vampire')
+# elif age > 100:
+#     print('You are not Alice, grannie.')
+
+# spam = 0
+
+# if spam < 5:
+#     print('Hello world.')
+#     spam = spam + 1
+    
+    
+# spam = 0
+
+# while spam < 5:
+#     print('Hello world.')
+#     spam = spam + 1
+
+# Annoying LOOP (Your name)
+
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+# print('Thank you!')
+
+#  BREAK
+# while True:
+#     print('Please type your name.')
+#     name = input()
+#     if name == 'your name':
+#         break
+# print('Thank you!')
