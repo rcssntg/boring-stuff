cd7b53c6fd7bf3d667710c822308e2efe817cbfe
rcssntg <rcssntg@gmail.com>
2024-08-14 23:59:38 +0800

    update learnList.py

--- learnList.py
+++ learnList.py
@@ -1,4 +1,19 @@
-spam = ['cat', 'bat', 'rat', 'elephant']
+# spam = ['cat', 'bat', 'rat', 'elephant']
 # print(spam[-1])
 # print(spam[-3])
-print(f'The {spam[-1]} is afraid of the {spam[-3]}.')
+# print(f'The {spam[-1]} is afraid of the {spam[-3]}.')
+
+
+# Sublists with slice
+
+# print(spam[0:4])
+# print(spam[1:3])
+# print(spam[0:-1])
+# print(spam[1:])
+# print(spam[:])
+
+
+# List's Length with len()
+
+spam =['cat', 'dog', 'moose']
+print(len(spam))
