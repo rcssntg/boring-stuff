24fbe879706db9c006d93e4326cde7e721e6c8d6
rcssntg <rcssntg@gmail.com>
2024-08-17 01:38:41 +0800

    add looplist.py

---
+++ looplist.py
@@ -0,0 +1,6 @@
+# for i in range(4):
+#     print(i)
+
+supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+for i in range(len(supplies)):
+    print(f'Index {i} in supplies is : {supplies[i]}')
