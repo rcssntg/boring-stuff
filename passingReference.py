2675c06f66b63608ad87482a03e14e983c93796e
rcssntg <rcssntg@gmail.com>
2024-08-19 00:19:12 +0800

    Learn about copy Module's copy() and deepcopy()

--- passingReference.py
+++ passingReference.py
@@ -1,6 +1,17 @@
-def eggs(someParameter):
-    someParameter.append('Hello')
+# def eggs(someParameter):
+#     someParameter.append('Hello')
 
-spam = [1, 2, 3]
-eggs(spam)
+# spam = [1, 2, 3]
+# eggs(spam)
+# print(spam)
+
+
+# copy Module's copy() and deepcopy() functions
+import copy
+
+spam =['A', 'B', 'C', 'D']
+cheese = copy.copy(spam)
+cheese[1] = 42
+
 print(spam)
+print(cheese)
