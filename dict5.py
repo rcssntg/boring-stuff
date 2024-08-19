98e7514781c78ce829e7d5fd0f406801ea8955df
rcssntg <rcssntg@gmail.com>
2024-08-19 22:44:17 +0800

    add dict5.py

---
+++ dict5.py
@@ -0,0 +1,16 @@
+# spam = {12345: 'Luggage Combination', 42: 'The Answer'}
+# print(spam[42])
+
+# DICTIONARIES VS LISTS
+
+# spam = ['cats', 'dogs', 'moose']
+# bacon = ['dogs', 'moose', 'cats']
+# print(spam==bacon)
+
+# eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+# ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+# print(eggs == ham)
+
+spam = {'name': 'Zophie', 'age': 7}
+print(spam['color']) # Expect KeyError
+
