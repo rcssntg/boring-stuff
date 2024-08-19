af33a82b974f8983dc8e9533db071ac143099032
rcssntg <rcssntg@gmail.com>
2024-08-19 22:20:30 +0800

    add heart image for loop

--- commaCode.py
+++ commaCode.py
@@ -1,19 +1,33 @@
-def format_list(items):
-    if len(items) == 0:
-        return ''
-    elif len(items) ==1:
-        return items[0]
-    else:
-        return ', '.join(items[:-1]) + ', and ' + items[-1]
+# def format_list(items):
+#     if len(items) == 0:
+#         return ''
+#     elif len(items) ==1:
+#         return items[0]
+#     else:
+#         return ', '.join(items[:-1]) + ', and ' + items[-1]
 
 
 
 
 
-spam = ['apples', 'bananas', 'tofu', 'cats', '42', 'True']
-# spam = []
-# spam = ['apples']
-result = format_list(spam)
-print(result)
+# spam = ['apples', 'bananas', 'tofu', 'cats', '42', 'True']
+# # spam = []
+# # spam = ['apples']
+# result = format_list(spam)
+# print(result)
 
-    
+grid = [['.', '.', '.', '.', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['.', 'O', 'O', 'O', 'O', 'O'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']]
+
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
