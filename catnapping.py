c8fa6946b3f0857295b22caaaa2c6d484f9ee0a4
rcssntg <rcssntg@gmail.com>
2024-08-26 22:04:04 +0800

    update catnapping.py

--- catnapping.py
+++ catnapping.py
@@ -1,6 +1,6 @@
 print('''Dear Alice,
       
-      Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
       
-      Sincerely,
-      Bob''')
+Sincerely,
+Bob''')
