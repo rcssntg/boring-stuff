283aa67775bbae5757de623d1911fd7ce259347c
rcssntg <rcssntg@gmail.com>
2024-08-26 22:33:35 +0800

    add catnapping.py and validateInput.py

--- catnapping.py
+++ catnapping.py
@@ -1,6 +1,35 @@
-print('''Dear Alice,
+# print('''Dear Alice,
       
-Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+# Eve's cat has been arrested for catnapping, cat burglary, and extortion.
       
-Sincerely,
-Bob''')
+# Sincerely,
+# Bob''')
+
+# def hello():
+#  print(len('Hello world!'))
+ 
+ 
+# hello()
+
+# spam = 'Hello world!'
+# fizz = spam[0:5]
+# print(fizz, spam)
+
+
+# spam ='Hello world!'
+# print(spam.upper())
+# print(spam.lower())
+# print(spam)
+
+print('How are you?')
+feeling = input()
+if feeling.lower() == 'great':
+    print('I feel greate too.')
+else:
+    print('I hope the rest of your day is good.')
+    
+# isalpha() returns True if the string consists only of letters and is not blank.
+# •  isalnum() returns True if the string consists only of letters and numbers
+# and is not blank.
+# •  isdecimal() returns True if the string consists only of numeric characters
+# and is not blank.
