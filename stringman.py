ab4e52ab5dd1a2cc4ad3babba8a2573cc0c5fc4e
rcssntg <rcssntg@gmail.com>
2024-08-26 22:01:51 +0800

    add stringman.py and catnapping.py

---
+++ stringman.py
@@ -0,0 +1,21 @@
+# spam = "That is ALice's Cat"
+# print(spam)
+
+
+# ESCAPE CHARACTERS
+# spam = 'Say hi to Bob\'s mother.'
+# print(spam)
+
+# Escape character      Prints as
+# \'                    Single quote
+# \"                    Double quote
+# \t                    Tab
+# \n                    Newline (line break)
+# \\                    Backslash
+
+# print("Hello there!\nHow are you?\nI\'m doing fine.")
+
+
+# Raw string
+# print(r'That is Carol\'s cat.') 
+
