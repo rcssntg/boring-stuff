32fe39ed4e5f019a077d0662a67edfa19bb4c57e
rcssntg <rcssntg@gmail.com>
2024-09-12 21:38:28 +0800

    update regex1.py with character classes

--- regex1.py
+++ regex1.py
@@ -67,3 +67,5 @@ print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
 phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)') # has groups
 print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
 
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge'))
